446c268bca37b38dbdf90a559ca808c04a418c65
susmit31 <51043859+susmit31@users.noreply.github.com>
2019-06-04 01:53:31 +0600

    Update index.js

--- index.js
+++ index.js
@@ -28,17 +28,22 @@ class Game{
 		//instantiating game objects
 		this.paddle = new Paddle(this);
 		
-		
 		this.ball = new Ball(this, 'img_ball', this.paddle);
 		
-		
 		this.bricks = [];
-		this.brickWidth = 50; //brick.width
-		this.brickCols = Math.floor(GAME_WIDTH/this.brickWidth);
-
-		for(let i=1; i<this.brickCols; i++){
-			for (let j=2; j<=5; j++){
-				this.bricks.push(new Brick(this,'img_brick', {x : i*50, y : j*25}));
+		this.brickWidth = 30; //brick.width
+		this.brickHeight = 15; //brick height
+		
+		//calculating starting brick number
+		this.brickCols = Math.floor(GAME_WIDTH/this.brickWidth) - deviceConst;
+		this.startBricks = this.brickCols * 7;
+		
+		this.scoreBoard = new Scoreboard(this);
+		
+		//adding bricks
+		for(let i=1; i <= this.brickCols; i++){
+			for (let j=2; j<=8; j++){
+				this.bricks.push(new Brick(this,'img_brick', {x : i*this.brickWidth, y : 25+j*this.brickHeight}));
 			}
 			
 		}
@@ -54,6 +59,8 @@ class Game{
 		for(let i=0; i<this.bricks.length; i++){
 			this.bricks[i].draw(ctx);
 		}
+		
+		this.scoreBoard.draw(ctx);
 		
 		//pause screen
 		if (this.gameState == GAMESTATE.PAUSED){
@@ -73,7 +80,6 @@ class Game{
 		//game over
 		if (this.gameState == GAMESTATE.GAMEOVER){
 			
-			
 			//shading up the game screen
 			ctx.fillStyle = 'rgba(0,0,0,0.5)';
 			ctx.fillRect(0,0, this.width, this.height);
@@ -81,11 +87,16 @@ class Game{
 			//writing 'game over'
 			ctx.fillStyle = 'tomato';
 			ctx.font = '50px "Germania One"';
-			//ctx.fontWeight = 'bold';
 			ctx.textAlign = 'center';
 			ctx.fillText('GAME OVER', this.width/2, this.height/2);
-			ctx.font = '20px "copperplate gothic bold"';
-			ctx.fillText('Press enter to restart', this.width/2, this.height/2+50);
+			
+			ctx.font = '20px "consolas"';
+			if('ontouchstart' in canvas){
+				ctx.fillText('Tap anywhere to restart', this.width/2, this.height/2+50);
+			}
+			else{
+				ctx.fillText('Press enter to restart', this.width/2, this.height/2+50);
+			}
 			
 		}
 		
@@ -110,11 +121,40 @@ class Game{
 	
 	update(deltaTime){
 		
+		//pausing
+		if(this.gameState == GAMESTATE.PAUSED) return;
+		
+		//game over
+		if(this.ball.position.y > this.height && this.gameState !== GAMESTATE.CLEARLEVEL) {
+			this.gameState = GAMESTATE.GAMEOVER;
+			let overSound = document.getElementById('over_sound');
+			if(overPlayCount == 0){
+				overSound.play();
+				overPlayCount += 1;
+			}
+			return;
+		}
+		
+		//level clear
+		if(this.bricks.length == 0) {
+			this.gameState = GAMESTATE.CLEARLEVEL;
+			let clearSound = document.getElementById('clear_sound');
+			if (clearPlayCount == 0){
+				clearSound.play();
+				clearPlayCount++;
+			}
+			return;
+		}
+
+	
 		//updating paddle
 		this.paddle.update(deltaTime);
 		
 		//updating ball
 		this.ball.update(deltaTime);
+		
+		//updating scoreBoard
+		this.scoreBoard.update();
 		
 		//updating bricks
 		for(let i=0; i<this.bricks.length; i++){
@@ -126,20 +166,11 @@ class Game{
 			return brick.markedForDeletion == false;
 		});
 		
-		if(this.bricks.length == 0) {
-			this.gameState = GAMESTATE.CLEARLEVEL;
-			return;
-		}
 		
-		//pausing the game
-		if(this.gameState == GAMESTATE.PAUSED) return;
+		//speeding up the ball
+		if (this.bricks.length < this.startBricks*2/3) this.ball.maxSpeed = 6;
 		
-		//checking for game over
-		if(this.ball.position.y > this.height) {
-			this.gameState = GAMESTATE.GAMEOVER;
-			return;
-		}
-	
+		
 	}
 	
 	togglePause(){
@@ -163,8 +194,8 @@ class Paddle{
 	//constructor
 	constructor(game){
 		//size
-		this.width = 120;
-		this.height = 13;
+		this.width = 80;
+		this.height = 10;
 		
 		//speed
 		this.maxSpeed = 10;
@@ -232,8 +263,12 @@ function detectCollision(ball, gameObj){
 		yMatchFromUp = (ball.position.y + ball.height) >= gameObj.position.y;
 	}
 	
-	
-	if (yMatchFromUp || yMatchFromDown) yMatch = true;
+	if (gameObj.width == 80){
+		if (yMatchFromUp) yMatch = true;
+	}
+	else {
+		if (yMatchFromUp || yMatchFromDown) yMatch = true;
+	}
 	
 	
 	if (xMatch && yMatch) return true;
@@ -263,7 +298,7 @@ function DesktopInputHandler(game){
 					
 			case 13:
 				if (game.gameState == GAMESTATE.GAMEOVER){
-					game.gameState = GAMESTATE.RUNNING;
+					location.reload();
 				}
 			}
 		});
@@ -306,7 +341,7 @@ function TouchInputHandler(game){
 		//distance of paddle from touch point
 		var vx = touchX - game.paddle.position.x - game.paddle.width/2;
 		//move paddle if more than 1px away from the touch point
-		if (vx > 1 || vx < 1) game.paddle.position.x += vx*EASING;
+		if (vx > 1 ) game.paddle.position.x += vx*EASING;
 		else game.paddle.position.x = touchX;
 		
 		
@@ -316,12 +351,31 @@ function TouchInputHandler(game){
 	
 
 	//handle ball
-	document.addEventListener('touchstart', event => {
-		if(game.ball.speed.y == 0){
-			game.ball.maxSpeed = 4
+	var timeout;
+	var lastTap = 0;
+	
+	document.addEventListener('touchend', event => {
+		
+		let currentTime = new Date().getTime();
+		let tapLength = currentTime - lastTap;
+		
+		if (tapLength > 0 && tapLength < 500){
 			game.ball.shootBall();
+			event.preventDefault();
+		}
+		
+		lastTap = currentTime;
+		
+	});
+	
+	
+	document.addEventListener('touchstart', event => {
+		if (game.gameState == GAMESTATE.GAMEOVER){
+			location.reload();
 		}
 	});
+	
+	
 		
 }
 
@@ -336,11 +390,11 @@ class Ball{
 	constructor(game, id){
 		
 		//size of the ball
-		this.width = 25;
-		this.height = 20;
+		this.width = 20;
+		this.height = 16;
 		
 		//speed of the ball
-		this.maxSpeed = 6;
+		this.maxSpeed = 4;
 		this.speed = {
 			x : 0,
 			y : 0
@@ -361,6 +415,10 @@ class Ball{
 	shootBall(){
 		this.speed.x = - this.maxSpeed;
 		this.speed.y = - this.maxSpeed;
+		
+		//playing shoot sound
+		let shootSound = document.getElementById('shootNbounce_sound');
+		shootSound.play();
 	}
 	
 	//draw the ball
@@ -371,7 +429,7 @@ class Ball{
 	}
 	
 	
-	//update method
+	//update the ball
 	update(deltaTime){
 		if (this.speed.y == 0){
 			this.position.x = game.paddle.position.x + this.width/2;
@@ -381,17 +439,34 @@ class Ball{
 			this.position.x += this.speed.x;
 			this.position.y += this.speed.y;
 			
+			let bounceSound = document.getElementById('shootNbounce_sound');
 			
 			//collision detection
-			if(this.position.x < 0 || this.position.x > game.width - this.width){
-				this.speed.x = -this.speed.x;
+			if(this.position.x < game.width%this.maxSpeed){
+				this.speed.x = this.maxSpeed;
+				if ((game.gameState !== GAMESTATE.GAMEOVER) || (game.gameState !==GAMESTATE.CLEARLEVEL)) bounceSound.play();
+			}
+				
+			if(this.position.x > game.width - this.width) {
+				this.speed.x = -this.maxSpeed;
+				if ((game.gameState !== GAMESTATE.GAMEOVER) || (game.gameState !==GAMESTATE.CLEARLEVEL)) bounceSound.play();
 			}
 			
-			if(this.position.y < 53) this.speed.y = -this.speed.y;
-			if(detectCollision(this,game.paddle) && this.speed.y!=0) this.speed.y = - this.maxSpeed;
+			if(this.position.y < 53) {
+				this.speed.y = this.maxSpeed;
+				if ((game.gameState !== GAMESTATE.GAMEOVER) || (game.gameState !==GAMESTATE.CLEARLEVEL)) bounceSound.play();
+			}
+			
+			if(detectCollision(this,game.paddle) && this.speed.y!=0){
+				this.speed.y = - this.maxSpeed;
+				if ((game.gameState !== GAMESTATE.GAMEOVER) || (game.gameState !==GAMESTATE.CLEARLEVEL)) bounceSound.play();
+			}
+			
 		}
 	}
 }
+
+
 
 
 
@@ -403,8 +478,8 @@ class Brick{
 		this.imgBrick = document.getElementById(id);
 		
 		//size of brick
-		this.width = 50; //game.brickWidth
-		this.height = 25;
+		this.width = 30; //game.brickWidth
+		this.height = 15;
 		
 		//position of brick
 		this.position = position;
@@ -423,9 +498,13 @@ class Brick{
 	//update brick's existence
 	update(deltaTime){
 		
+		//breaking sound
+		let breakSound = document.getElementById('break_sound');
+		
 		if(detectCollision(game.ball, this)){
 			game.ball.speed.y = -game.ball.speed.y;
 			this.markedForDeletion = true;
+			breakSound.play();
 		}
 	}
 	
@@ -433,23 +512,84 @@ class Brick{
 
 
 
+
+
+//scoreboard class
+class Scoreboard{
+	
+	constructor(game){
+		
+		this.startBricks = game.startBricks;
+		this.localStorageName = 'brickbreaker-game';
+	
+	}
+	
+	update(){
+		
+		//finding out current score
+		this.currentBricks = game.bricks.length;
+		
+		this.score = (this.startBricks - this.currentBricks) * 50;
+		
+		//checking if there is any data in the localStorage
+		//if there isn't, high score is set to 0
+		//otherwise, high score is set to as found in localStorage
+		if (localStorage.getItem(this.localStorageName) == null) highScore = 0;
+		else highScore = localStorage.getItem(this.localStorageName);
+		
+		//comparing existing high score and current score
+		highScore = Math.max(this.score, highScore);
+		localStorage.setItem(this.localStorageName, highScore);
+	}
+	
+	draw(ctx){
+		
+		ctx.fillStyle = 'rgb(255,200,165)';
+		ctx.fillRect(0, 0, game.width, 50);
+		
+		ctx.fillStyle = 'tomato';
+		ctx.font = '25px "Luckiest Guy"';
+		ctx.textAlign = 'center';
+		ctx.fillText("CURRENT SCORE: " + this.score, game.width/2, 20);
+		ctx.fillText("BEST SCORE: " + highScore, game.width/2, 45);
+		
+	}
+	
+}
+
 // getting the canvas and the context
 let canvas = document.getElementById('gameScreen');
 let ctx = canvas.getContext('2d');
 
-
-//setting the height and width of the canvas and the game
-canvas.width = window.innerWidth*0.95;
-canvas.height = window.innerHeight*0.95;
+//setting the height and width of the game
+var deviceConst = 0;
+//for phones
+if('ontouchstart' in canvas){
+	canvas.width = window.innerWidth*0.95;
+	canvas.height = window.innerHeight*0.95;
+	deviceConst = 1;
+}
+//for desktops
+else{
+	canvas.height = 600;
+	canvas.width = 800;
+	deviceConst = 2;
+}
 
 let GAME_HEIGHT = canvas.height;
 let GAME_WIDTH = canvas.width;
 
+
+//clearing game screen
 ctx.clearRect(0,0,GAME_WIDTH,GAME_HEIGHT);
 
 
+//declaring sound play counters
+let overPlayCount = 0;
+let clearPlayCount = 0;
 
 //starting the game
+var highScore;
 let game = new Game(GAME_WIDTH, GAME_HEIGHT);
 game.start();
 
